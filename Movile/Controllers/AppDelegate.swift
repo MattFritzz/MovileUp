fc597c719977d71d0c3c6dfeb13a76ef1bc87e50
iOS <ios@MAC-134.nabs.com.br>
2015-07-15 14:42:21 -0300

    Initial Commit

---
+++ Movile/Controllers/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  Movile
+//
+//  Created by iOS on 7/15/15
+//  Copyright (c) 2015 Movile. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
+}
